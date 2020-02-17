76148724baa7ef69d38037aae5072ab65c98d4e1
khhh97 <596073386@qq.com>
2020-02-17 18:38:42 +0800

    test: github webhook 测试
    github webhook 测试

--- app/router.js
+++ app/router.js
@@ -7,4 +7,5 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.get('/api', controller.home.index);
+  router.get('/api/a', controller.home.index);
 };
